05a35a497d280568da8463f34114982034555bd4
dlof-lib <javadlof8@gmail.com>
2026-07-15 02:40:40 +0300

    Initial commit from ZIP upload

---
+++ SLCD/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SLCD"
+include(":slcd-app")
+include(":core")
