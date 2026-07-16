bc7f84f6d1f0fdf9ded9e9a541c135a7937b8894
dlof-lib <javadlof8@gmail.com>
2026-07-16 23:19:34 +0300

    Initial commit from ZIP upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("com.android.library") version "8.5.2" apply false
+    // AGP 8.7.x يدعم Android 15 (API 35) بشكل مستقر: compileSdk/targetSdk = 35،
+    // محاذاة المكتبات الأصلية بحجم صفحة 16 كيلوبايت تلقائياً، ودعم كامل لواجهات
+    // البرمجة الجديدة (predictive back، edge-to-edge الإلزامي).
+    id("com.android.application") version "8.7.2" apply false
+    id("com.android.library") version "8.7.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
